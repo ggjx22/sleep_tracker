c18d277d1e3032bf0d2627a82539e2e7bf23f56d
ggjx22 <navlag18011@gmail.com>
2023-12-14 21:57:22 +0800

    #21 entry point of the streamlit app

--- 1_🏡_Home.py
+++ 1_🏡_Home.py
@@ -1,14 +1,17 @@
 import src.utils_styling as style
 import streamlit as st
 
-st.set_page_config(
-    page_title='Sleeping',
-    page_icon='😴'
-)
+def main():
+    st.set_page_config(
+        page_title='Sleeping',
+        page_icon='😴'
+    )
 
-style.title('Welcome to the Sleep Tracker Web App.')
+    style.title('Welcome to the Sleep Tracker Web App.')
+    style.text("It's a litte boring now. Stay tuned for more features!")
 
-style.text("It's a litte boring now. Stay tuned for more features!")
-
-# web app version
-style.text('Web app release version 1.3.2')
+    # web app version
+    style.text('Web app release version 1.3.2')
+    
+if __name__ == '__main__':
+    main()
