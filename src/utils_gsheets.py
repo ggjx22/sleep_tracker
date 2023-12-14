e2429e0f3de9f91ced4f4e05afbc101b4762357d
ggjx22 <navlag18011@gmail.com>
2023-12-14 21:09:53 +0800

    improve #21 provide documentation for class GSheetsManager

--- src/utils_gsheets.py
+++ src/utils_gsheets.py
@@ -2,13 +2,31 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 class GSheetsManager:
+    '''
+    Class to interact with google sheets.
+    '''
     def __init__(self):
         self.conn = self.init_connection()
         
     def init_connection(self):
+        '''
+        Method that initializes connection with google sheets.
+        '''
         return st.connection('gsheets', type=GSheetsConnection)
     
     def fetch_data(self, worksheet_name=None):
+        '''
+        Method that fetch data from google sheets.
+        
+        Parameters:
+        worksheet_name (str): Name of the worksheet to fetch data from.
+        
+        Returns:
+        data (DataFrame): Dataframe containing the data from the worksheet.
+        
+        Raises:
+        ValueError: If worksheet_name is not provided.
+        '''
         if worksheet_name is None:
             raise ValueError('Please provide a worksheet name.')
         
@@ -18,6 +36,16 @@ class GSheetsManager:
         return data
     
     def update_data(self, updated_data, worksheet_name=None):
+        '''
+        Method that updates data in google sheets.
+        
+        Parameters:
+        updated_data (DataFrame): Dataframe containing the updated data.
+        worksheet_name (str): Name of the worksheet to update data in.
+        
+        Raises:
+        ValueError: If worksheet_name is not provided.
+        '''
         if worksheet_name is None:
             raise ValueError('Please provide a worksheet name.')
         
